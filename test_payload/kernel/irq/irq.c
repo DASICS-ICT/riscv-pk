e52a6d26b9d5e8e650a58de9c3e015d2b87c3444
Jin Yue <jinyue20s@ict.ac.cn>
2023-06-09 16:45:30 +0800

    [feature] irq: add dasics exception handler

--- test_payload/kernel/irq/irq.c
+++ test_payload/kernel/irq/irq.c
@@ -4,6 +4,7 @@
 #include <os/string.h>
 #include <kio.h>
 #include <kassert.h>
+#include <kcsr.h>
 #include <arch/sbi.h>
 #include <drivers/screen.h>
 #include "encoding.h"
@@ -64,8 +65,38 @@ void init_exception()
     }
     irq_table[IRQC_S_TIMER] = handle_int;
     exc_table[EXCC_SYSCALL] = handle_syscall;
+    exc_table[EXCC_DASICS_SINSTR_FAULT] = handle_dasics;
+    exc_table[EXCC_DASICS_SLOAD_FAULT] = handle_dasics;
+    exc_table[EXCC_DASICS_SSTORE_FAULT] = handle_dasics;
     delegate_dasics();
     setup_exception();
+}
+
+
+void handle_dasics(regs_context_t *regs, uint64_t stval, uint64_t cause)
+{
+    switch (cause)
+    {
+        case EXCC_DASICS_SINSTR_FAULT:
+            printk("[HANDLE_DASICS]: Detect SInst Access Fault! Skip this instruction!\n");
+            break;
+        case EXCC_DASICS_SLOAD_FAULT:
+            printk("[HANDLE_DASICS]: Detect SLoad Access Fault! Skip this instruction!\n");
+            break;
+        case EXCC_DASICS_SSTORE_FAULT:
+            printk("[HANDLE_DASICS]: Detect SStore Access Fault! Skip this instruction!\n");
+            break;
+        default:
+            printk("[HANDLE_DASICS]: Invalid cause 0x%lx detected!\n");
+            printk("sstatus: 0x%lx sbadaddr: 0x%lx scause: %lu\n\r",
+                regs->sstatus, regs->sbadaddr, regs->scause);
+            printk("sepc: 0x%lx\n\r", regs->sepc);
+            sbi_shutdown();
+            break;
+    }
+
+    regs->sepc += 4;
+
 }
 
 void handle_other(regs_context_t *regs, uint64_t stval, uint64_t cause)
