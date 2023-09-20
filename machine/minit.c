08d6e902fffa739e6d114b0a6805f6cace562116
wanghan192 <wanghan192@mails.ucas.ac.cn>
2023-09-20 16:21:16 +0800

    restore H extension

--- machine/minit.c
+++ machine/minit.c
@@ -69,7 +69,7 @@ static void delegate_traps()
 
   write_csr(mideleg, interrupts);
   write_csr(medeleg, exceptions);
-  assert(read_csr(mideleg) == interrupts);
+  assert(read_csr(mideleg) & interrupts == interrupts);
   assert(read_csr(medeleg) == exceptions);
 
   if(!supports_extension('N'))
